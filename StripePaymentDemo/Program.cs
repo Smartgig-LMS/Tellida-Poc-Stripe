d3419bd3e18fe1bddfc82f0d6fd13ffeb9e46a33
Avula ANITHA <anitha.avula@smartgig.tech>
2024-05-28 17:31:12 +0530

    code is working with sample data

--- StripePaymentDemo/Program.cs
+++ StripePaymentDemo/Program.cs
@@ -1,14 +1,32 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Security.Policy;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddControllers();
 builder.Services.Configure<StripeOptions>(builder.Configuration.GetSection("Stripe"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles(); // Enable serving static files from wwwroot
+
+app.UseRouting();
 
 app.UseAuthorization();
 
@@ -16,8 +34,10 @@ app.MapControllers();
 
 app.Run();
 
+// Class to bind Stripe options from appsettings.json
 public class StripeOptions
 {
     public string SecretKey { get; set; }
     public string WebhookSecret { get; set; }
 }
+
