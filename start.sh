cc4d4cb0f8ce2cc212a59fba203fba65bedeb383
bharathkumar-sg <114640350+bharathkumar-sg@users.noreply.github.com>
2024-06-17 13:20:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Stop and remove the existing container if it exists
-sudo docker-compose -f RazorPay_Payment/docker-compose.yaml down
+#sudo docker-compose -f RazorPay_Payment/docker-compose.yaml down
 
 # Remove the previous Docker image named stripeimage3
-sudo docker rmi razorpayimage
+#sudo docker rmi razorpayimage
 
 # Build the Docker image in the StripePaymentDemo directory
 sudo docker build -t razorpayimage -f RazorPay_Payment/Dockerfile RazorPay_Payment
